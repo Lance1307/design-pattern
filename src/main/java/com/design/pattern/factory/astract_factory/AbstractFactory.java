038645ddd06cbee4850dbda3f9d7eb1235a05bdc
Lance1307 <our130727@126.com>
2019-08-29 22:27:21 +0800

    设计模式之抽象工厂和静态工厂

---
+++ src/main/java/com/design/pattern/factory/astract_factory/AbstractFactory.java
@@ -0,0 +1,6 @@
+package com.design.pattern.factory.astract_factory;
+
+public class AbstractFactory {
+
+
+}
---
+++ src/main/java/com/design/pattern/factory/simple_factory/AnimalFactory.java
@@ -0,0 +1,7 @@
+package com.design.pattern.factory.simple_factory;
+
+public class AnimalFactory {
+
+
+
+}
